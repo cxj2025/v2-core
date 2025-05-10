ace7a00a54ffebc776a170c2fab90dbaa1bba8c2
cxj2025 <37360624@qq.com>
2025-05-10 11:16:33 +0800

    Update UQ112x112.sol

--- contracts/libraries/UQ112x112.sol
+++ contracts/libraries/UQ112x112.sol
@@ -1,4 +1,6 @@
-pragma solidity >=0.8.10;
+pragma solidity >=0.8.0;
+
+// SPDX-License-Identifier: Apache-2.0
 
 // a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
 
